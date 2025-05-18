9a4ae1669252a6042d3d527e6b100b308aab9723
Mazerous <amolketin@gmail.com>
2025-05-18 14:15:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+document.querySelectorAll('nav a').forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector(e.target.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+  });
+});
 
